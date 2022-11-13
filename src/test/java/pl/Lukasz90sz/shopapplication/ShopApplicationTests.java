2051f662b05470d43b8373f1abc53ea2ca67cdff
name <email>
2022-11-13 14:16:57 +0100

    initial commit

---
+++ src/test/java/pl/Lukasz90sz/shopapplication/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.Lukasz90sz.shopapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
